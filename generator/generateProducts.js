7ad871f5c60fa4cc9b62d3797bfdd12fd1484c05
Ryan Shin <ryanminjaeshin@gmail.com>
2020-10-19 16:14:25 -0700

    Import CSV to postgres

--- generator/generateProducts.js
+++ generator/generateProducts.js
@@ -3,8 +3,8 @@ const faker = require('faker');
 const fs = require('fs');
 
 const records = 1;
-const csv = fs.createWriteStream('products.csv');
-csv.write('product_id,product_name,images,image_ids,image_descriptions\n');
+const csv = fs.createWriteStream('productsInfo.csv');
+csv.write('product_id|product_name|images|image_ids|image_descriptions\n');
 
 const generateData = (file, callback) => {
   let count = 0;
@@ -15,10 +15,10 @@ const generateData = (file, callback) => {
       count += 1;
       const product_id = count;
       const product_name = faker.commerce.productName();
-      const images = JSON.stringify(['https://unzwillingimg.s3-us-west-1.amazonaws.com/images/main.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/1.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/2.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/3.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/4.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/5.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/6.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/7.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/8.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/9.jpg']);
-      const image_ids = JSON.stringify([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-      const image_descriptions = JSON.stringify(new Array(10).fill(null).map(() => faker.fake('{{lorem.sentence}}')));
-      const data = `${product_id},${product_name},${images},${image_ids},${image_descriptions}\n`;
+      const images = (['https://unzwillingimg.s3-us-west-1.amazonaws.com/images/main.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/1.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/2.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/3.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/4.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/5.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/6.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/7.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/8.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/9.jpg']).toString();
+      const image_ids = ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).toString();
+      const image_descriptions = (new Array(10).fill(null).map(() => faker.fake('{{lorem.sentence}}'))).toString();
+      const data = `${product_id}|${product_name}|{${images}}|{${image_ids}}|{${image_descriptions}}\n`;
       if (count === records) {
         file.write(data, callback);
       } else {
--- schema.sql
+++ schema.sql
@@ -1,19 +1,35 @@
-DROP DATABASE IF EXIST lego;
+DROP DATABASE IF EXISTS lego;
 
 CREATE DATABASE lego;
 
-USE lego;
+\c lego;
 
-CREATE TABLE products (
+CREATE TABLE productsInfo (
   product_id SERIAL PRIMARY KEY,
   product_name TEXT UNIQUE NOT NULL,
+  images TEXT [],
+  image_ids NUMERIC [],
+  image_descriptions TEXT []
 );
 
-CREATE TABLE images (
-  image_id SERIAL PRIMARY KEY,
-  product_id INT,
-  image TEXT NOT NULL,
-  image_description TEXT UNIQUE NOT NULL,
-  FOREIGN KEY (product_id)
-  REFERENCES products (product_id)
-);
+-- CREATE TABLE products (
+--   product_id SERIAL PRIMARY KEY,
+--   product_name TEXT UNIQUE NOT NULL
+-- );
+
+-- CREATE TABLE images (
+--   image_id SERIAL PRIMARY KEY,
+--   product_id INT,
+--   image TEXT NOT NULL,
+--   image_description TEXT UNIQUE NOT NULL,
+--   FOREIGN KEY (product_id)
+--     REFERENCES products (product_id)
+-- );
+
+COPY productsInfo (product_id, product_name, images, image_ids, image_descriptions) FROM '/Users/rms/Desktop/HR/hrsjo/SDC/gallery/productsInfo.csv'DELIMITER '|'CSV HEADER;
+
+-- COPY master_table (product_id, product_name) TO 'products.csv';
+
+-- COPY products.info (department, category, subcategory, brand, price, id) TO 'recommendations.csv';
+
+-- COPY products.recommendations (department, category, subcategory, brand, price, id) FROM 'recommendations.csv';
