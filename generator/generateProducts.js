cd9cb3c7ca4d41a7ff22f6bcd79edc03f80af2da
Ryan Shin <ryanminjaeshin@gmail.com>
2020-10-19 11:59:29 -0700

    Update data generation script

--- generator/generateProducts.js
+++ generator/generateProducts.js
@@ -2,9 +2,9 @@
 const faker = require('faker');
 const fs = require('fs');
 
-const records = 100;
+const records = 1;
 const csv = fs.createWriteStream('products.csv');
-csv.write('product_id,product_name,image,image_id,image_description\n');
+csv.write('product_id,product_name,images,image_ids,image_descriptions\n');
 
 const generateData = (file, callback) => {
   let count = 0;
@@ -15,10 +15,10 @@ const generateData = (file, callback) => {
       count += 1;
       const product_id = count;
       const product_name = faker.commerce.productName();
-      const image = faker.image.imageUrl();
-      const image_id = count;
-      const image_description = faker.lorem.sentence();
-      const data = `${product_id},${product_name},${image},${image_id},${image_description}\n`;
+      const images = JSON.stringify(['https://unzwillingimg.s3-us-west-1.amazonaws.com/images/main.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/1.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/2.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/3.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/4.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/5.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/6.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/7.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/8.jpg', 'https://unzwillingimg.s3-us-west-1.amazonaws.com/images/9.jpg']);
+      const image_ids = JSON.stringify([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+      const image_descriptions = JSON.stringify(new Array(10).fill(null).map(() => faker.fake('{{lorem.sentence}}')));
+      const data = `${product_id},${product_name},${images},${image_ids},${image_descriptions}\n`;
       if (count === records) {
         file.write(data, callback);
       } else {
